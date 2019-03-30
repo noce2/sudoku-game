76f707e8690fe0514036e5478992b7a5b392ff4c
Nsikan <nsikane@live.co.uk>
2019-03-29 22:00:07 -0400

    Add unit tests for generateSudokuGrid function

--- sudoku-game-infrastructure/generateSudokuGrid/index.ts
+++ sudoku-game-infrastructure/generateSudokuGrid/index.ts
@@ -2,14 +2,13 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
 
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: "Hello " + "here is your grid"
     };
 
-    context.done();
+    Promise.resolve(context.done());
 };
 
 export default httpTrigger;
--- sudoku-game-infrastructure/tests/generateSudokuGrid/index.spec.ts
+++ sudoku-game-infrastructure/tests/generateSudokuGrid/index.spec.ts
@@ -1,14 +1,22 @@
 import { Context } from "@azure/functions";
 import { expect } from "chai";
+import { mock, when, anything, anyString } from "ts-mockito";
 
 import httpTrigger from "../../generateSudokuGrid/index";
+import { defaultContextObject } from "../defaultContextObject";
 
 describe("generateSudokuGrid", () => {
-    let mockContextObject: Context;
     
     it("should return a json containing non-null grid", (done) => {
-        httpTrigger(mockContextObject, {})
-        expect(mockContextObject.res).to.be.not.null;
-        done();
+        let mockContextObject: Context = mock(defaultContextObject);
+        when(mockContextObject.log(anyString)).thenCall(console.log);
+
+        Promise.resolve(httpTrigger(mockContextObject, {}))
+        .then(() => {
+            expect(mockContextObject.res).to.be.not.null;
+            expect(mockContextObject.res).to.have.property("grid");
+            done();
+        })
+        .catch(err => done(err));
     })
 })
