f5defef38bdde4eadc911255579ab920c2818e23
Nsikan <nsikane@live.co.uk>
2019-03-31 00:55:22 -0400

    Add tests for setting cell values

--- sudoku-game-infrastructure/generateSudokuGrid/cell.ts
+++ sudoku-game-infrastructure/generateSudokuGrid/cell.ts
@@ -1,5 +1,18 @@
 import { SudokuNumber } from "./sudokunumber.enum";
 
 export class Cell {
-    entry: SudokuNumber;
+    _entry: SudokuNumber;
+
+    get entry():SudokuNumber {
+        return this._entry;
+    }
+
+    set entry(number: SudokuNumber) {
+        // the below only works because TypeScript allows reverse mappings
+        if(SudokuNumber[number]){
+            this._entry = number;
+        } else {
+            throw new Error("invalid sudoku number");
+        }
+    }
 }
--- sudoku-game-infrastructure/test/generateSudokuGrid/cell.spec.ts
+++ sudoku-game-infrastructure/test/generateSudokuGrid/cell.spec.ts
@@ -4,12 +4,22 @@ import { SudokuNumber } from "../../generateSudokuGrid/sudokunumber.enum";
 import { Cell } from "../../generateSudokuGrid/cell";
 
 describe("Cell", () => {
-    it("should be initialised with an empty entry", () => {
+    it("should be initialised with an empty entry if no args supplied", () => {
         const testCell: Cell = new Cell();
         expect(testCell.entry).to.be.undefined;
-    })
+    });
     
-    it("should allow any number between 1 and 9 to be set", () => {
-        throw(new Error("no test written yet"));
+    it("should allow any integer between 1 and 9 to be set", () => {
+        const testCell: Cell = new Cell();
+        testCell.entry = 3;
+        expect(testCell.entry).to.eql(3);
+    });
+
+    it("should not allow any integer outside 1 and 9 to be set", () => {
+        const testCell: Cell = new Cell();
+        
+        expect(() => {testCell.entry = 0}).to.throw(/invalid sudoku number/);
+        expect(() => {testCell.entry = -1}).to.throw(/invalid sudoku number/);
+        expect(() => {testCell.entry = 2.2}).to.throw(/invalid sudoku number/);
     })
 })
