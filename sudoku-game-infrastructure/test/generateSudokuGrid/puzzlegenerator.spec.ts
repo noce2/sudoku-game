7cde20709dce4430929e067a09bfab591044fc9a
Nsikan Essien <nsikane@live.co.uk>
2019-06-02 22:21:11 +0100

    Add empty test suite for puzzle generator service

---
+++ sudoku-game-infrastructure/test/generateSudokuGrid/puzzlegenerator.spec.ts
@@ -0,0 +1,11 @@
+import { expect } from "chai"
+
+import { PuzzleGeneratorService } from "../../generateSudokuGrid/puzzlegeneratorservice";
+
+describe("Puzzle Generator Service", () => {
+    it("", () => {
+        const testService: PuzzleGeneratorService = new PuzzleGeneratorService();
+        expect().equals();
+        expect().equals();
+    });
+})
