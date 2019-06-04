b11dd677a52c305d72115a8da8656c3cca227021
Nsikan Essien <nsikane@live.co.uk>
2019-06-04 07:55:59 +0100

    Rename puzzlegenerator service

--- sudoku-game-web/generateSudokuGrid/puzzlegenerator.service.ts
+++ sudoku-game-web/generateSudokuGrid/puzzlegenerator.service.ts
@@ -2,5 +2,8 @@ import { Grid } from "./grid";
 
 // TODO: Add a singleton decorator to ensure only one of this exists
 export class PuzzleGeneratorService {
+    generateInitialGrid() {
+        throw new Error("Method not implemented.");
+    }
 
 }
